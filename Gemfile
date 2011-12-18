3ee76cd323eddd47222ee6343834f466263130cf
coolzilj <coolzilj@msn.com>
2011-12-18 17:46:43 +0800

    user model with passwords , and user show page

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.3'
-
+gem 'gravatar_image_tag'
 gem 'sqlite3'
 
 group :assets do
@@ -14,7 +14,7 @@ gem 'jquery-rails'
 
 group :development do
 	gem 'rspec-rails','2.6.1'
-	gem 'annotate'
+
 end
 
 group :test do 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,27 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validate :name, :presence => true,
                   :length => {:maximum => 50}
   validate :email, :presence => true,
                    :format => {:with => email_regex},
                    :uniqueness => {:case_sensitive => false }
+  
+  def password
+    @password
+  end
+  
+  def password=(pass)
+    return unless pass
+    @password = pass
+    generate_password(pass)
+    
+  end
+  
+  private
+  def generate_password(pass)
+    salt = Array.new(10){rand(1024).to_s(36)}.join
+    self.salt,self.hashed_password = 
+      salt,Digest::SHA256.hexdigest(pass+salt)
+  end
 end
