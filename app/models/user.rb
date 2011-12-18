da0b25d7b3105dc8459d8bf4f204cae077974573
coolzilj <coolzilj@msn.com>
2011-12-18 18:42:48 +0800

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validate :name, :presence => true,
+  
+  validates :name, :presence => true,
                   :length => {:maximum => 50}
-  validate :email, :presence => true,
+  validates :email, :presence => true,
                    :format => {:with => email_regex},
-                   :uniqueness => {:case_sensitive => false }
+                   :uniqueness => true
   
   def password
     @password
