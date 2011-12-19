2fd64fd577c7daa300367690b5b89953d433e095
coolzilj <coolzilj@msn.com>
2011-12-20 00:49:38 +0800

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   attr_accessible :name,:email,:password,:password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id", :class_name => "Relationship", :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -18,11 +22,23 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   
   def feed
-    Micropost.where("user_id=?",id)
+    Micropost.from_users_followed_by(self)
   end
   
   def has_password?(submitted_password)
     hashed_password == encrypt(submitted_password)
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed) 
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
   private
@@ -53,4 +69,6 @@ class User < ActiveRecord::Base
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
+  
+
 end
